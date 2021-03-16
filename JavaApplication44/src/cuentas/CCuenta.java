c2e4359f57767baabf4fde7abf9325abfdec2f33
PERSONAL <PERSONAL@LAPTOP-62PVFCJU>
2021-03-16 12:18:06 +0100

    Quarta  modificación

--- JavaApplication44/src/cuentas/CCuenta.java
+++ JavaApplication44/src/cuentas/CCuenta.java
@@ -1,37 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/*1. La clase forma parte del paquete cuentas*/
+/*Este proyecto esta reflejado los ejercicios 1,2,3,4,5,6,7,8 y 9 */
+/*https://github.com/kikinha2021/proyecto1.git*/
+
+
 package cuentas;
 
 /**
  *
- * @author PERSONAL
+ * @author Erika Machado de Lima
+ * @version 1.0 febrero_2021
  */
+
+
 public class CCuenta {
   
- private String nombre;
+     /*Se instancia las variables utilando private */
+    private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
+  
+      /*Constructor por defecto */
     public CCuenta()
     {
     }
 
     
-    //________________Ejercicio 4 _______________________
-    // Se encapsula los atributos de la classe CCuenta
-    // Se verifica que las variables estan encapsuladas utilizando private 
-    // Se genera los metodos get y setter 
-  public CCuenta(String nombre, String cuenta, double saldo, double tipoInterés) {
+      /*Constructor por parametros*/
+     
+     /**      
+      * @param nombre  nombre de la persona
+      * @param cuenta se refiere la cuenta 
+      * @param saldo  el saldo que tendra el cliente 
+      * @param tipoInterés el tiepo de interes de la cuenta
+      */
+    
+    public CCuenta(String nombre, String cuenta, double saldo, double tipoInterés) {
     this.nombre = nombre;
     this.cuenta = cuenta;
     this.saldo = saldo;
     this.tipoInterés = tipoInterés;
   }
 
+   /*Añadimos los getter y setter */
   public String getNombre() {
     return nombre;
   }
@@ -67,9 +80,10 @@ public class CCuenta {
 
     
     
-    
-    
-    
+    /**
+     * *@return .Devuelve el saldo actual
+     * 
+     */
     
 
     public double estado()
@@ -77,13 +91,28 @@ public class CCuenta {
         return saldo;
     }
 
+    
+       /*@throws Exception , ha una llamada a Exception y lanzara la excepcion correspodiente
+       *al error encontrado
+        */
+    
+       /**
+       * @param cantidad hace referencia la cantidad que va ingresar la cuenta 
+       *
+       */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
-
+    
+  
+    
+    
+       /**
+       * @param cantidad hace referencia la cantidad que va retirar la cuenta 
+       */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -93,21 +122,37 @@ public class CCuenta {
         saldo = saldo - cantidad;
     }
     
-    //_______________Ejercicio 3_______________
-    //_______________Ejercicio 5_______________
-    // - Introducir operativa_cuenta que englobe las sentencias de la clase Main que operan con
-    // el objeto cuenta1 - He creado un metodo cualquiera solo para interagir con el objeto en la clase Main
-    // Añadimos un nuevo parametro al metodo operativa_cantidad y de tipo float 
+    
+    /*3. Introduccion el metodo operativa_cuenta*/
+    /*5. Se añade un parametro cantida tipo float y creamos algo para probar con el objeto*/
     public void  operativa_cuenta(double cantidad){
       
       System.out.println("Datos de la cuenta ");
+      
+      /*. Creo una salida aqui para probar las modificacione con git*/
       System.out.println("prueba para Git ");
       System.out.println("Nombre del cliente "+nombre);
       System.out.println("Cuenta Bancaria "+cuenta);
       System.out.println("Su saldo actual es : "+saldo);
       
+      if( cantidad > 1000) {
+        
+        System.out.println("Has ultrapasado su limite de retirada, maximo 1000");
+      }
+      
+      else {
+        saldo = saldo - cantidad;
+        System.out.println("Su saldo actual es"+saldo);
+      }
+      
+     
+      
+      
     }
    
+    public void prueba(){
+      
+    }
+    
     
 }
-
